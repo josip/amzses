e1e61292f9ceb92f831f0ba551df450915941aca
Tom Lieber <tom@alltom.com>
2012-07-08 16:49:44 -0700

    encapsulate in struct; InitAuth allows specifying access key and secret key as arguments

--- amzses.go
+++ amzses.go
@@ -26,35 +26,40 @@ const (
 	endpoint = "https://email.us-east-1.amazonaws.com"
 )
 
-var accessKey, secretKey string
-
-func init() {
-	config := jconfig.LoadConfig("/etc/aws.conf")
-	accessKey = config.GetString("aws_access_key")
-	secretKey = config.GetString("aws_secret_key")
+type SES struct {
+	accessKey, secretKey string
 }
 
-func SendMail(from, to, subject, body string) (string, error) {
+func Init() *SES {
+	config := jconfig.LoadConfig("/etc/aws.conf")
+	return &SES{config.GetString("aws_access_key"), config.GetString("aws_secret_key")}
+}
+
+func InitAuth(accessKey, secretKey string) *SES {
+	return &SES{accessKey, secretKey}
+}
+
+func (ses *SES) SendMail(from, to, subject, body string) (string, error) {
 	data := make(url.Values)
 	data.Add("Action", "SendEmail")
 	data.Add("Source", from)
 	data.Add("Destination.ToAddresses.member.1", to)
 	data.Add("Message.Subject.Data", subject)
 	data.Add("Message.Body.Text.Data", body)
-	data.Add("AWSAccessKeyId", accessKey)
+	data.Add("AWSAccessKeyId", ses.accessKey)
 
-	return sesGet(data)
+	return ses.sesGet(data)
 }
 
-func authorizationHeader(date string) []string {
-	h := hmac.New(sha256.New, []uint8(secretKey))
+func (ses *SES) authorizationHeader(date string) []string {
+	h := hmac.New(sha256.New, []uint8(ses.secretKey))
 	h.Write([]uint8(date))
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", accessKey, signature)
+	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", ses.accessKey, signature)
 	return []string{auth}
 }
 
-func sesGet(data url.Values) (string, error) {
+func (ses *SES) sesGet(data url.Values) (string, error) {
 	urlstr := fmt.Sprintf("%s?%s", endpoint, data.Encode())
 	endpointURL, _ := url.Parse(urlstr)
 	headers := map[string][]string{}
@@ -64,10 +69,10 @@ func sesGet(data url.Values) (string, error) {
 	date := now.Format("Mon, 02 Jan 2006 15:04:05 -0700")
 	headers["Date"] = []string{date}
 
-	h := hmac.New(sha256.New, []uint8(secretKey))
+	h := hmac.New(sha256.New, []uint8(ses.secretKey))
 	h.Write([]uint8(date))
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", accessKey, signature)
+	auth := fmt.Sprintf("AWS3-HTTPS AWSAccessKeyId=%s, Algorithm=HmacSHA256, Signature=%s", ses.accessKey, signature)
 	headers["X-Amzn-Authorization"] = []string{auth}
 
 	req := http.Request{
